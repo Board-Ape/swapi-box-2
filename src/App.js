f23451237f48102f886459b623cd397ec369f5a7
Sam Singer <samuel.singer1@gmail.com>
2017-12-06 13:47:08 -0700

    Iteration-0 Crawl Data Randomly Appearing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      crawlFilm: ''
+    };
+  }
+
+  async componentDidMount() {
+    const initialFetch = await fetch('https://swapi.co/api/films/');
+    const responseFilmData = await initialFetch.json();
+    const crawlFilmData = responseFilmData.results.map( film => film.opening_crawl );
+    const crawlFilm = this.randomlyGenerateCrawl(crawlFilmData);
+    this.setState({crawlFilm})
+  }
+
+  randomlyGenerateCrawl(crawlFilmData) {
+    const randomNumber = Math.floor((Math.random() * crawlFilmData.length));
+    return crawlFilmData[randomNumber];
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{this.state.crawlFilm}</p>
       </div>
     );
   }

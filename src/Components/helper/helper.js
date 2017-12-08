35838b5f40f92591624e128f35dc93bc5ef82c95
Sam Singer <samuel.singer1@gmail.com>
2017-12-08 00:10:57 -0700

    FilmCrawl present and broken out into component

---
+++ src/Components/helper/helper.js
@@ -0,0 +1,9 @@
+const fetchFilmTextCrawl = (rawData) => {
+  return {
+    title: "THE FORCE AWAKENS",
+    date: "December 11, 2015",
+    episode: "VII",
+    crawl: ['Luke Skywalker has vanished. In his absence, the sinister FIRST ORDER has risen from the ashes of the Empire and will not rest until Skywalker, the last Jedi, has been destroyed.', 'With the support of the REPUBLIC, General Leia Organa leads a brave RESISTANCE. She is desperate to find her brother Luke and gain his help in restoring peace and justice to the galaxy.', 'Leia has sent her most daring pilot on a secret mission to Jakku, where an old ally has discovered a clue to Lukes whereabouts....'] }
+};
+
+export default fetchFilmTextCrawl

742166b9044012198503a1593d22b6c4fecbd37a
Sam Singer <samuel.singer1@gmail.com>
2017-12-09 13:13:36 -0700

    Refactor CardContainer

--- src/Components/CardContainer/CardContainer.js
+++ src/Components/CardContainer/CardContainer.js
@@ -7,44 +7,56 @@ const CardContainer = ({ containerTitle,
 													active,
 													items }) => {
 
-		const displayChars = characters.map((character, index) => {
-			return <Card name={character.name}
-									 lineOne={character.homeworld}
-									 lineTwo={character.species}
-									 lineThree={character.homePop}
-									 key={index}
-									 type={0}
-	                 favoriteCardFunc={favoriteCardFunc} />
-		})
+		// const displayChars = characters.map((character, index) => {
+		// 	return <Card name={character.name}
+		// 							 lineOne={character.homeworld}
+		// 							 lineTwo={character.species}
+		// 							 lineThree={character.homePop}
+		// 							 key={index}
+		// 							 type={0}
+	  //                favoriteCardFunc={favoriteCardFunc} />
+		// })
+		//
+		// const displayWorlds = worlds.map((world, index) => {
+		// 	return <Card name={world.name}
+		// 							 lineOne={world.terrain}
+		// 							 lineTwo={world.population}
+		// 							 lineThree={world.climate}
+		// 							 lineFour={world.residents.join(', ')}
+		// 							 key={index}
+		// 							 type={1}
+		// 							 favoriteCardFunc={favoriteCardFunc} />
+		// })
+		//
+		// const displayVehicles = vehicles.map((vehicle, index) => {
+		// 	return <Card name={vehicle.name}
+		// 							 lineOne={vehicle.model}
+		// 							 lineTwo={vehicle.vClass}
+		// 							 lineThree={vehicle.passengers}
+		// 							 key={index}
+		// 							 type={2}
+		// 							 favoriteCardFunc={favoriteCardFunc} />
+		// })
 
-		const displayWorlds = worlds.map((world, index) => {
-			return <Card name={world.name}
-									 lineOne={world.terrain}
-									 lineTwo={world.population}
-									 lineThree={world.climate}
-									 lineFour={world.residents.join(', ')}
-									 key={index}
-									 type={1}
-									 favoriteCardFunc={favoriteCardFunc} />
-		})
+	let displayItems = items.filter(item => item.cardType === active)
 
-		const displayVehicles = vehicles.map((vehicle, index) => {
-			return <Card name={vehicle.name}
-									 lineOne={vehicle.model}
-									 lineTwo={vehicle.vClass}
-									 lineThree={vehicle.passengers}
-									 key={index}
-									 type={2}
-									 favoriteCardFunc={favoriteCardFunc} />
-		})
+	let currrentDisplay = displayItems.map((item) => {
+		return <Card name={item.name}
+									lineOne={item.lineOne}
+									lineTwo={item.lineTwo}
+									lineThree={item.lineThree}
+									lineFour={item.lineFour}
+									key={item.id}
+									id={item.id}
+									cardType={item.cardType}
+									favorite={favoriteCard} />
+	})
 
 	return (
 		<div className="card-container-component">
 			<h2>{containerTitle}</h2>
 			<div className="card-container">
-				{displayChars}
-				{displayWorlds}
-				{displayVehicles}
+				{currrentDisplay}
 			</div>
 		</div>
 	)

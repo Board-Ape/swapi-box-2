d26c7a580492b1fc99e7f8cad6f15f78343b5d2e
Sam Singer <samuel.singer1@gmail.com>
2017-12-08 22:03:04 -0700

    All fetch calls are pulling correct objects

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import './Header.css';
 
-const Header = ({favorites, getWorlds, handleUpdateState}) => {
+const Header = ({favorites, getWorlds, getVehicles, handleUpdateState}) => {
   return (
     <div className='header-component'>
       <h1>Star-Swapi-Box</h1>
       <h3>Favorites <span>{favorites.length}</span></h3>
       <div>
-        <button onClick={() => {handleUpdateState('characters')}}>Character</button>
-        <button onClick={() => {handleUpdateState('worlds')}}>Worlds</button>
+        <button onClick={() => handleUpdateState('characters')}>Characters</button>
+        <button onClick={() => handleUpdateState('worlds')}>Worlds</button>
+				<button onClick={() => handleUpdateState('vehicles')}>Vehicles</button>
       </div>
     </div>
   );
 };
 
 export default Header;
-
-// <button onClick={props.fetchWorlds}>Worlds</button>
-// <button onClick={props.fetchVehicles}>Vehicles</button>
